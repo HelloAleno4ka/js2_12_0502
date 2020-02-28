a03c980f7189b1963edaa27002be93e879f13304
HelloAleno4ka <aaay85@mail.ru>
2020-02-28 20:38:04 +0200

    components

---
+++ 6/src/public/componenst/search.vue
@@ -0,0 +1,22 @@
+<template>
+    <form action="#" class="search-form">
+                    <input type="text" class="search-field">
+                    <button class="btn-search" type="submit">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </form>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            search_text: ""
+        }
+    },
+    watch: {
+        search_text: function (val) {            
+        this.$parent.search_text = val.toLowerCase()
+        }
+  }
+}
+</script>
